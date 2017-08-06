3ef3acf95ab1b0bbb627dbdc774d178c6ebe8feb
Ava-O <octavia.stancu@gmail.com>
2017-08-06 12:18:21 +0300

    Added deliberately failing test for lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
